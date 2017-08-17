37f194cf3c1b80bf0d67be893bbe023b5a1e88f0
roneysousa <roney@infog2.com.br>
2017-08-16 22:23:04 -0300

    Adição da página lista de usuarios

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,6 @@
 from app import app
 from app.models.tables import User
-from bottle import request, template, static_file
+from bottle import request, template, static_file, redirect
 
 # static routes
 @app.get('/<filename:re:.*\.css>')
@@ -21,7 +21,7 @@ def fonts(filename):
 
 @app.route('/') # @get('/login')
 def login():
-    return template('login')
+    return template('login', sucesso=True)
 
 @app.route('/cadastro')
 def cadastro():
@@ -33,14 +33,26 @@ def acao_cadastro(db):
     password = request.forms.get('password')
     new_user = User(username, password)
     db.add(new_user)
-    return template('verificacao_cadastro', nome=username)
+    #return template('lista_usuarios')
+    return redirect('/usuarios')
 
 @app.route('/', method='POST') # @post('/')
-def acao_login():
+def acao_login(db):
     username = request.forms.get('username')
     password = request.forms.get('password')
-    return template('verificacao_login', sucesso=True, nome=username)
+    result = db.query(User).filter((User.username==username) \
+        & (User.password==password)).all()
+    if result:
+        return redirect('/usuarios')
+        #return template('lista_usuarios')
+    return template('login', sucesso=False)
+    #return template('verificacao_login', sucesso=True, nome=username)
     #return template('views/verificacao_login', sucesso=check_login(username, password), nome=username)
+
+@app.route('/usuarios')
+def usuarios(db):
+    usuarios = db.query(User).all()
+    return template('lista_usuarios', usuarios=usuarios)
 
 @app.error(404)
 def error404(error):
